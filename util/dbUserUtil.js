57b749c856118b7097b2a85a4ec3f34a1861c569
Mark Colby <macolby14@gmail.com>
2021-04-03 12:40:28 -1000

    fix - empty item error when creating new user in db

--- util/dbUserUtil.js
+++ util/dbUserUtil.js
@@ -1,6 +1,13 @@
-export async function createUserInDb(db, email, checklistToAdd = {}) {
-  const result = await db
-    .collection("users")
-    .insertOne({ email, checklists: { 1: checklistToAdd } });
-  return result;
+export async function createUserInDb(db, email, checklistToAdd) {
+  if (checklistToAdd === undefined) {
+    const result = await db
+      .collection("users")
+      .insertOne({ email, checklists: {} });
+    return result;
+  } else {
+    const result = await db
+      .collection("users")
+      .insertOne({ email, checklists: { 1: checklistToAdd } });
+    return result;
+  }
 }
