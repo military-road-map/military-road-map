813f4627e8b3bc4ae71debb930371f4b3a56f780
Mark Colby <macolby14@gmail.com>
2021-04-03 01:02:27 -1000

    prettier cleanup

--- util/populdateDb.js
+++ util/populdateDb.js
@@ -2,32 +2,41 @@
  * Method to standardize creating text
  * @param {string} text
  * @param {boolean} isComplete
- * @param {number} minTimeFromTarget  
- * @param {number} maxTimeFromTarget  
+ * @param {number} minTimeFromTarget
+ * @param {number} maxTimeFromTarget
  */
 
-function createListItem(text="", isComplete=false, minTimeFromTarget=0, maxTimeFromTarget=0){
-    return {
-        text,
-        isComplete,
-        minTimeFromTarget,
-        maxTimeFromTarget
-    };
+function createListItem(
+  text = "",
+  isComplete = false,
+  minTimeFromTarget = 0,
+  maxTimeFromTarget = 0
+) {
+  return {
+    text,
+    isComplete,
+    minTimeFromTarget,
+    maxTimeFromTarget,
+  };
 }
 
 /**
  * Method to standardize creating a list
  * @param {string} name
- * @param {string} targetDate 
- * @param {{}} tasks  
+ * @param {string} targetDate
+ * @param {{}} tasks
  */
 
-function createList(name="Example List", targetDate="2022-01-01T00:00:00", tasks ={}){
-    return {
-        name,
-        targetDate,
-        tasks
-    }
+function createList(
+  name = "Example List",
+  targetDate = "2022-01-01T00:00:00",
+  tasks = {}
+) {
+  return {
+    name,
+    targetDate,
+    tasks,
+  };
 }
 
 /**
@@ -36,9 +45,9 @@ function createList(name="Example List", targetDate="2022-01-01T00:00:00", tasks
  * @param {item[]} checklists
  */
 
-function createUser(email="example@gmail.com", checklists=[]){
-    return {
-        email,
-        checklists
-    }
+function createUser(email = "example@gmail.com", checklists = []) {
+  return {
+    email,
+    checklists,
+  };
 }
