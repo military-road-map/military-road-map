84c3b05bb9946124bd8c956964d02bd7139681b5
Mark Colby <macolby14@gmail.com>
2021-04-03 00:50:12 -1000

    start populateDb file

---
+++ util/populdateDb.js
@@ -0,0 +1,46 @@
+/**
+ * Method to standardize creating text
+ * @param {string} text
+ * @param {boolean} isComplete
+ * @param {number} minTimeFromTarget  
+ * @param {number} maxTimeFromTarget  
+ */
+
+function createListItem(text="", isComplete=false, minTimeFromTarget=0, maxTimeFromTarget=0){
+    return {
+        text,
+        isComplete,
+        minTimeFromTarget,
+        maxTimeFromTarget
+    };
+}
+
+/**
+ * Method to standardize creating a list
+ * @param {string} name
+ * @param {type} id
+ * @param {string} targetDate 
+ * @param {{}} tasks  
+ */
+
+function createList(name="Example List", targetDate="2022-01-01T00:00:00", tasks ={}){
+    return {
+        name,
+        type,
+        eventDate,
+        tasks
+    }
+}
+
+/**
+ * Method to standardize creating a user
+ * @param {string} email
+ * @param {item[]} checklists
+ */
+
+function createUser(email="example@gmail.com", checklists=[]){
+    return {
+        email,
+        checklists
+    }
+}
