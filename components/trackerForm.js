7b565140c6ee74862ceb1003c2af1f7a0ac828ce
Mark Colby <macolby14@gmail.com>
2021-04-03 23:59:57 -1000

    cleanup - move tracker form code to separate file

---
+++ components/trackerForm.js
@@ -0,0 +1,116 @@
+import styled from "styled-components";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const FormWrapperStyle = styled.div`
+  border-top: 2px solid black;
+
+  form {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    flex-direction: column;
+    margin: 0 24px;
+    row-gap: 8px;
+    column-gap: 8px;
+  }
+
+  label {
+    text-align: right;
+  }
+
+  button {
+    width: fit-content;
+    margin: auto;
+    grid-column-start: span 2;
+  }
+
+  .error {
+    color: red;
+    font-size: var(--size-6);
+    margin-left: 8px;
+    display: inline-block;
+  }
+`;
+
+export function TrackerForm({ checklistTemplates, handleAddChecklist }) {
+  return (
+    <FormWrapperStyle>
+      <Formik
+        initialValues={{
+          checklistName: "",
+          checklistInd: "",
+          checklistDate: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.checklistName) {
+            errors.checklistName = "Required";
+          }
+          if (!values.checklistInd) {
+            errors.checklistInd = "Required";
+          }
+          if (!values.checklistDate) {
+            errors.checklistDate = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          console.log("submitting");
+          await handleAddChecklist(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label htmlFor="checklistName">Task Name:</label>
+            <div>
+              <Field
+                type="text"
+                name="checklistName"
+                placeholder="My Checklist Name"
+              />
+              <ErrorMessage
+                className="error"
+                name="checklistName"
+                component="div"
+              />
+            </div>
+            <label htmlFor="checklistInd">Choose a checklist:</label>
+            <div>
+              <Field as="select" name="checklistInd">
+                <option value="">Select One...</option>
+                {!checklistTemplates
+                  ? null
+                  : Object.entries(checklistTemplates).map(
+                      ([key, checklist]) => {
+                        return (
+                          <option key={key} value={key}>
+                            {checklist.name}
+                          </option>
+                        );
+                      }
+                    )}
+              </Field>
+              <ErrorMessage
+                name="checklistInd"
+                component="div"
+                className="error"
+              />
+            </div>
+            <label>Your target task date:</label>
+            <div>
+              <Field type="date" name="checklistDate" />
+              <ErrorMessage
+                name="checklistDate"
+                component="div"
+                className="error"
+              />
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </FormWrapperStyle>
+  );
+}
--- pages/tracker.js
+++ pages/tracker.js
@@ -6,7 +6,7 @@ import { ChecklistContext } from "../components/contextAndProvider/context";
 import useSWR from "swr";
 import { updateUserChecklists } from "../util/dbUserUtil";
 import { useSession } from "next-auth/client";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { TrackerForm } from "../components/trackerForm";
 
 const ListHeader = styled.div`
   display: flex;
@@ -27,36 +27,6 @@ const IndividualList = styled(ListHeader)`
 
   :hover {
     background-color: hsl(140deg, 100%, 30%, 0.7);
-  }
-`;
-
-const FormWrapperStyle = styled.div`
-  border-top: 2px solid black;
-
-  form {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    flex-direction: column;
-    margin: 0 24px;
-    row-gap: 8px;
-    column-gap: 8px;
-  }
-
-  label {
-    text-align: right;
-  }
-
-  button {
-    width: fit-content;
-    margin: auto;
-    grid-column-start: span 2;
-  }
-
-  .error {
-    color: red;
-    font-size: var(--size-6);
-    margin-left: 8px;
-    display: inline-block;
   }
 `;
 
@@ -157,115 +127,10 @@ const Page = () => {
           {checklistSpot}
         </section>
         <h2>Create a new Tracker</h2>
-        <FormWrapperStyle>
-          {/* <form>
-            <label>
-              Task Name:
-              <input
-                id="checklistName"
-                type="text"
-                placeholder="Enter Checklist Name"
-                autoComplete="off"
-              ></input>
-            </label>
-            <select id="checklistType">
-              <option value="none">Select One...</option>
-              <optgroup label="Checklists">
-                {!checklistTemplates
-                  ? null
-                  : Object.entries(checklistTemplates).map(
-                      ([key, checklist]) => {
-                        return (
-                          <option key={key} value={key}>
-                            {checklist.name}
-                          </option>
-                        );
-                      }
-                    )}
-              </optgroup>
-            </select>
-            <input id="date" type="date"></input>
-            <button onClick={handleAddChecklist}>Submit</button>
-          </form> */}
-
-          <Formik
-            initialValues={{
-              checklistName: "",
-              checklistInd: "",
-              checklistDate: "",
-            }}
-            validate={(values) => {
-              const errors = {};
-              if (!values.checklistName) {
-                errors.checklistName = "Required";
-              }
-              if (!values.checklistInd) {
-                errors.checklistInd = "Required";
-              }
-              if (!values.checklistDate) {
-                errors.checklistDate = "Required";
-              }
-              return errors;
-            }}
-            onSubmit={async (values, { setSubmitting }) => {
-              console.log("submitting");
-              await handleAddChecklist(values);
-              setSubmitting(false);
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <label htmlFor="checklistName">Task Name:</label>
-                <div>
-                  <Field
-                    type="text"
-                    name="checklistName"
-                    placeholder="My Checklist Name"
-                  />
-                  <ErrorMessage
-                    className="error"
-                    name="checklistName"
-                    component="div"
-                  />
-                </div>
-                <label htmlFor="checklistInd">Choose a checklist:</label>
-                <div>
-                  <Field as="select" name="checklistInd">
-                    <option value="">Select One...</option>
-                    {!checklistTemplates
-                      ? null
-                      : Object.entries(checklistTemplates).map(
-                          ([key, checklist]) => {
-                            return (
-                              <option key={key} value={key}>
-                                {checklist.name}
-                              </option>
-                            );
-                          }
-                        )}
-                  </Field>
-                  <ErrorMessage
-                    name="checklistInd"
-                    component="div"
-                    className="error"
-                  />
-                </div>
-                <label>Your target task date:</label>
-                <div>
-                  <Field type="date" name="checklistDate" />
-                  <ErrorMessage
-                    name="checklistDate"
-                    component="div"
-                    className="error"
-                  />
-                </div>
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-          </Formik>
-        </FormWrapperStyle>
+        <TrackerForm
+          handleAddChecklist={handleAddChecklist}
+          checklistTemplates={checklistTemplates}
+        ></TrackerForm>
       </TrackerStyle>
     </Layout>
   );
