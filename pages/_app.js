128276c0b0c514fcef0119fbc35a19eca49e8ee1
Mark Colby <macolby14@gmail.com>
2021-04-04 07:46:44 -1000

    add Operation On Track favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Provider } from "next-auth/client";
+import Head from "next/head";
 import ContextProvider from "../components/contextAndProvider/ContextProvider";
 import "./styles.css";
 
@@ -6,29 +7,35 @@ import "./styles.css";
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }) {
   return (
-    <Provider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      options={{
-        // Client Max Age controls how often the useSession in the client should
-        // contact the server to sync the session state. Value in seconds.
-        // e.g.
-        // * 0  - Disabled (always use cache value)
-        // * 60 - Sync session state with server if it's older than 60 seconds
-        clientMaxAge: 0,
-        // Keep Alive tells windows / tabs that are signed in to keep sending
-        // a keep alive request (which extends the current session expiry) to
-        // prevent sessions in open windows from expiring. Value in seconds.
-        //
-        // Note: If a session has expired when keep alive is triggered, all open
-        // windows / tabs will be updated to reflect the user is signed out.
-        keepAlive: 0,
-      }}
-      session={pageProps.session}
-    >
-      <ContextProvider>
-        <Component {...pageProps} />
-      </ContextProvider>
-    </Provider>
+    <>
+      <Head>
+        <title>Operation On Track</title>
+        <link rel="icon" href="/favicon.svg"></link>
+      </Head>
+      <Provider
+        // Provider options are not required but can be useful in situations where
+        // you have a short session maxAge time. Shown here with default values.
+        options={{
+          // Client Max Age controls how often the useSession in the client should
+          // contact the server to sync the session state. Value in seconds.
+          // e.g.
+          // * 0  - Disabled (always use cache value)
+          // * 60 - Sync session state with server if it's older than 60 seconds
+          clientMaxAge: 0,
+          // Keep Alive tells windows / tabs that are signed in to keep sending
+          // a keep alive request (which extends the current session expiry) to
+          // prevent sessions in open windows from expiring. Value in seconds.
+          //
+          // Note: If a session has expired when keep alive is triggered, all open
+          // windows / tabs will be updated to reflect the user is signed out.
+          keepAlive: 0,
+        }}
+        session={pageProps.session}
+      >
+        <ContextProvider>
+          <Component {...pageProps} />
+        </ContextProvider>
+      </Provider>
+    </>
   );
 }
