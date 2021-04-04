7bb60457de077a63d7b5a2fc8e51a649728b0132
Mark Colby <macolby14@gmail.com>
2021-04-03 17:30:58 -1000

    better sample data for separation checklist

---
+++ exampleData/monthsToMs.js
@@ -0,0 +1,11 @@
+function monthToMillisecond(month) {
+  // month*#month/yr*#day/month*#hr/day*min/hr*sec/min*ms/sec
+  return month * 12 * 30 * 24 * 60 * 60 * 1000;
+}
+
+console.log(monthToMillisecond(24));
+console.log(monthToMillisecond(18));
+console.log(monthToMillisecond(12));
+console.log(monthToMillisecond(6));
+console.log(monthToMillisecond(4));
+console.log(monthToMillisecond(3));
