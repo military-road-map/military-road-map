3009c095c483306a686c245ee930657960de29f4
Mark Colby <macolby14@gmail.com>
2021-04-03 20:38:20 -1000

    add basic styling to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,41 @@
+import styled from "styled-components";
+import Layout from "../components/layout";
+
+const IndexStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+
+  h2 {
+    font-size: var(--size-3);
+    text-align: center;
+    padding-bottom: 16px;
+  }
+
+  p {
+    font-size: var(--size-4);
+    padding-bottom: 8px;
+  }
+`;
+
 export default function index() {
-  return <div>Home Page</div>;
+  return (
+    <Layout>
+      <IndexStyles>
+        <h1>Operation On Track</h1>
+        <h2>Your military one-stop resource to never miss a milestone again</h2>
+        <p>
+          There are hundres of items to track while you’re in the miltiary. VA
+          appointments, transition appointments, PCS timelines... on top of your
+          regular job.
+        </p>
+        <p>
+          Don’t be the service member who misses out on benefits or a program
+          until its too late.
+        </p>
+        <button>Get Started</button>
+      </IndexStyles>
+    </Layout>
+  );
 }
